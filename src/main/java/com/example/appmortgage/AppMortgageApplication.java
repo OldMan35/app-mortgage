e8dd014325fdf9ff390e343658d11efbed8d3cb4
OldMan35 <kostya_kv@mail.ru>
2021-08-04 21:14:50 +0300

    Initial commit

---
+++ src/main/java/com/example/appmortgage/AppMortgageApplication.java
@@ -0,0 +1,13 @@
+package com.example.appmortgage;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AppMortgageApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AppMortgageApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/appmortgage/AppMortgageApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.appmortgage;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AppMortgageApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
