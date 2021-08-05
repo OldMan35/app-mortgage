53380802669061ac87aac1b4e83a98fadb494e02
OldMan35 <kostya_kv@mail.ru>
2021-08-06 00:47:32 +0300

    refactor MortgageClients.java

--- src/main/java/com/example/appmortgage/model/MortgageClients.java
+++ src/main/java/com/example/appmortgage/model/MortgageClients.java
@@ -3,8 +3,6 @@ package com.example.appmortgage.model;
 import lombok.Data;
 
 import javax.persistence.*;
-import javax.validation.constraints.Max;
-import javax.validation.constraints.Min;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
 
