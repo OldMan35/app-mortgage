c5e26bdf79807a99216735fcd74bc2e0a0a4f17c
OldMan35 <kostya_kv@mail.ru>
2021-08-08 00:27:25 +0300

    fix bags

--- src/test/java/com/example/appmortgage/AppMortgageApplicationTests.java
+++ src/test/java/com/example/appmortgage/AppMortgageApplicationTests.java
@@ -5,7 +5,6 @@ import com.example.appmortgage.controller.MortgageController;
 import com.example.appmortgage.model.MortgageClients;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -13,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.MvcResult;
-import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.get;
