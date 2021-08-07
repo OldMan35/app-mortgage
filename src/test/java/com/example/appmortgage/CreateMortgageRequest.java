e9891c28632d8be1720a29c7974f8c0dccbf950a
OldMan35 <kostya_kv@mail.ru>
2021-08-07 18:50:49 +0300

    fix bags

--- src/test/java/com/example/appmortgage/CreateMortgageRequest.java
+++ src/test/java/com/example/appmortgage/CreateMortgageRequest.java
@@ -12,7 +12,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.test.web.servlet.MockMvc;
-import org.springframework.test.web.servlet.RequestBuilder;
 
 
 import static org.springframework.test.web.client.match.MockRestRequestMatchers.content;
@@ -33,8 +32,8 @@ public class CreateMortgageRequest {
     @Autowired
     private ObjectMapper objectMapper;
 
-    @Autowired
-    private MortgageClientsRepository mortgageClientsRepository;
+//    @Autowired
+//    private MortgageClientsRepository mortgageClientsRepository;
 
     //сброс БД после теста
 //    @AfterEach
