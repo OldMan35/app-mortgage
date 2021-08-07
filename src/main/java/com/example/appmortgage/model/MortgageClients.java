2958f1370741705b3ceb54aae8005ed0fb403b7a
OldMan35 <kostya_kv@mail.ru>
2021-08-08 01:20:39 +0300

    fix bags

--- src/main/java/com/example/appmortgage/model/MortgageClients.java
+++ src/main/java/com/example/appmortgage/model/MortgageClients.java
@@ -3,13 +3,11 @@ package com.example.appmortgage.model;
 
 import lombok.Getter;
 import lombok.Setter;
-
 import javax.persistence.*;
 import javax.validation.constraints.NotNull;
 
 @Getter
 @Setter
-@NotNull
 @Entity
 @Table(name = "mortgage_clients")
 public class MortgageClients {
@@ -38,54 +36,71 @@ public class MortgageClients {
     }
 
     @Id
+    @NotNull
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
 
+    @NotNull
     @Column(name = "name_of_buyers")
     private String nameOfBuyers;
 
+    @NotNull
     @Column(name = "patronymic_of_buyers")
     private String patronymicOfBuyers;
 
+    @NotNull
     @Column(name = "surname_of_buyers")
     private String surnameOfBuyers;
 
+    @NotNull
     @Column(name = "phone_number_of_buyers")
     private String phoneNumberOfBuyers;
 
+    @NotNull
     @Column(name = "loan_amount")
     private long loanAmount;
 
+    @NotNull
     @Column(name = "loan_term")
     private int loanTerm;
 
+    @NotNull
     @Column(name = "estate_object")
     private String estateObject;
 
+    @NotNull
     @Column(name = "cost_object")
     private long costObject;
 
+    @NotNull
     @Column(name = "inn_of_buyers")
     private String innOfBuyers;
 
+    @NotNull
     @Column(name = "name_of_sellers")
     private String nameOfSellers;
 
+    @NotNull
     @Column(name = "patronymic_of_sellers")
     private String patronymicOfSellers;
 
+    @NotNull
     @Column(name = "surname_of_sellers")
     private String surnameOfSellers;
 
+    @NotNull
     @Column(name = "inn_ind")
     private String innInd;
 
+    @NotNull
     @Column(name = "name_organization")
     private String nameOrganization;
 
+    @NotNull
     @Column(name = "inn_org")
     private String innOrg;
 
+    @NotNull
     @Column(name = "own_rights")
     private String ownRights;
 }
